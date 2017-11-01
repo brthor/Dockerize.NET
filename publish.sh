9e7abaf8db9da06a747b6fc95c98aebc8d5dcb23
Bryan Thornbury <bt.mozoby@gmail.com>
2017-10-31 17:16:27 -0700

    will this owrk?

--- publish.sh
+++ publish.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -ev
 
+echo "${TRAVIS_PULL_REQUEST}"
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+	echo "Publishing Dockerize.NET $(printf %05d $TRAVIS_BUILD_NUMBER)"
+	
 	dotnet pack -c Release --version-suffix $(printf %05d $TRAVIS_BUILD_NUMBER)
 	dotnet nuget push ./bin/Release/Brthor.Dockerize.NET.*.nupkg -s https://www.myget.org/F/thor/api/v2/package -k $MYGET_KEY
 fi
