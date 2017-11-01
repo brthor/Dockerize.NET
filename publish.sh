47bc485a0bfe7d9ae019cd301768e6104b1098f8
Bryan Thornbury <bt.mozoby@gmail.com>
2017-11-01 00:20:22 -0700

    publish

--- publish.sh
+++ publish.sh
@@ -3,8 +3,9 @@ set -ev
 
 echo "${TRAVIS_PULL_REQUEST}"
 echo "$TRAVIS_PULL_REQUEST_BRANCH"
+echo "$TRAVIS_BRANCH"
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
 	echo "Publishing Dockerize.NET $(printf %05d $TRAVIS_BUILD_NUMBER)"
 
 	dotnet pack -c Release --version-suffix $(printf %05d $TRAVIS_BUILD_NUMBER)
