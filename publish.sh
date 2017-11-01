219b0a08db11b143df0bab035b6b67294848c9be
Bryan Thornbury <brthor@users.noreply.github.com>
2017-11-01 00:24:39 -0700

    Find a way to fix the publish script (#5)
    * will this owrk?
    
    * wtf
    
    * publish

--- publish.sh
+++ publish.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -ev
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+echo "${TRAVIS_PULL_REQUEST}"
+echo "$TRAVIS_PULL_REQUEST_BRANCH"
+echo "$TRAVIS_BRANCH"
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+	echo "Publishing Dockerize.NET $(printf %05d $TRAVIS_BUILD_NUMBER)"
+
 	dotnet pack -c Release --version-suffix $(printf %05d $TRAVIS_BUILD_NUMBER)
 	dotnet nuget push ./bin/Release/Brthor.Dockerize.NET.*.nupkg -s https://www.myget.org/F/thor/api/v2/package -k $MYGET_KEY
 fi
