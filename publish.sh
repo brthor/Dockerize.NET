ee1c48c484f9a1d9daa038d72b295e5e69acf4da
Bryan Thornbury <bt.mozoby@gmail.com>
2017-10-31 17:21:44 -0700

    wtf

--- publish.sh
+++ publish.sh
@@ -2,10 +2,11 @@
 set -ev
 
 echo "${TRAVIS_PULL_REQUEST}"
+echo "$TRAVIS_PULL_REQUEST_BRANCH"
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	echo "Publishing Dockerize.NET $(printf %05d $TRAVIS_BUILD_NUMBER)"
-	
+
 	dotnet pack -c Release --version-suffix $(printf %05d $TRAVIS_BUILD_NUMBER)
 	dotnet nuget push ./bin/Release/Brthor.Dockerize.NET.*.nupkg -s https://www.myget.org/F/thor/api/v2/package -k $MYGET_KEY
 fi
