6ad8a3a11ddc416e78ce03be47ad2b217756037f
Bryan Thornbury <bt.mozoby@gmail.com>
2017-11-03 11:25:08 -0700

    tweak publishing

--- publish.sh
+++ publish.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -ev
 
-echo "${TRAVIS_PULL_REQUEST}"
-echo "$TRAVIS_PULL_REQUEST_BRANCH"
-echo "$TRAVIS_BRANCH"
+package_name="Brthor.Dockerize.NET"
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-	echo "Publishing Dockerize.NET $(printf %05d $TRAVIS_BUILD_NUMBER)"
+    echo "Publishing ${package_name} $(printf %05d $TRAVIS_BUILD_NUMBER)"
 
-	dotnet pack -c Release --version-suffix $(printf %05d $TRAVIS_BUILD_NUMBER)
-	dotnet nuget push ./bin/Release/Brthor.Dockerize.NET.*.nupkg -s https://www.myget.org/F/thor/api/v2/package -k $MYGET_KEY
+    dotnet pack -c Release --version-suffix $(printf %05d $TRAVIS_BUILD_NUMBER)
+    dotnet nuget push ./bin/Release/${package_name}*.nupkg -k $NUGET_KEY
 fi
